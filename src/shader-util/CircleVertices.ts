4ee9275cc6de6d645572a5d99f8689d7814e5ea0
lee mighdoll <lee@underneath.ca>
2023-03-20 23:48:06 -0700

    formatter

--- src/shader-util/CircleVertices.ts
+++ src/shader-util/CircleVertices.ts
@@ -1,5 +1,5 @@
-import { Vec2 } from "./Vec";
 import { mapN, partitionBySize } from "thimbleberry/shader-util";
+import { Vec2 } from "./Vec";
 
 /** a circle with vertices between [-1,1] centered at 0,0 */
 export function circleVerts(n: number): Vec2[] {
@@ -15,7 +15,7 @@ export function circleVerts(n: number): Vec2[] {
 }
 
 /** @returns a triangle strip for a circle with with vertices between [-1,1] centered at 0,0
- * @param numCircleVerts the number of verticies on the circumference 
+ * @param numCircleVerts the number of verticies on the circumference
  */
 export function circleStrip(numCircleVerts: number): Vec2[] {
   const center: Vec2 = [0, 0];
