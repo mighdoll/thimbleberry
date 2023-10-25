763e8242d9575af7c41d380bc4cf4ff0c9de14c2
lee mighdoll <lee@underneath.ca>
2023-10-24 23:34:45 -0700

    support dynamic csv column widths

--- src/shader-util/FormattedCsv.ts
+++ src/shader-util/FormattedCsv.ts
@@ -1,12 +1,17 @@
-/** width of each column */
-export interface ColumnDescription {
-  [key: string]: number;
-}
-
-/** values for every column in a row */
+/** values for one row */
 export type ColumnValues<T> = {
   [Property in keyof T]: string;
 };
+
+/** width of each column. Undefined widths will calculated dynamically. */
+export interface ColumnDescription {
+  [key: string]: number | undefined | null;
+}
+
+/** static width of each column */
+interface ColumnWidths {
+  [key: string]: number;
+}
 
 /** utility class to print column aligned tabular csv data */
 export class FormattedCsv<T extends ColumnDescription> {
@@ -16,22 +21,40 @@ export class FormattedCsv<T extends ColumnDescription> {
     this.fields = fields;
   }
 
-  header(): string {
-    const keyValues = Object.keys(this.fields).map(k => [k, k]);
-    const o = Object.fromEntries(keyValues);
-    return this.row(o);
+  /** @returns the csv tabular report for the provided array of values */
+  report(values: ColumnValues<T>[]): string {
+    const headerEntries = Object.keys(this.fields).map(k => [k, k]);
+    const header = Object.fromEntries(headerEntries);
+    const jsonRows = [header, ...values];
+    const columns = this.fixedColumns(values);
+    const stringRows = jsonRows.map(r => this.row(r, columns));
+    return stringRows.join("\n");
   }
 
-  row(values: ColumnValues<T>): string {
-    const row = Object.entries(this.fields).map(([name, width]) => {
+  /** generate string for one row in the table */
+  private row(values: ColumnValues<T>, columnWidths: ColumnWidths): string {
+    const row = Object.entries(columnWidths).map(([name, width]) => {
       const value = values[name];
       return value.slice(0, width).padStart(width, " ");
     });
     return row.join(",");
   }
 
-  report(rowValues: ColumnValues<T>[]): string {
-    const rows = rowValues.map(r => this.row(r));
-    return [this.header(), ...rows].join("\n");
+  /** calculate column widths dynamically for unspecified columns widths */
+  private fixedColumns(values: ColumnValues<T>[]): ColumnWidths {
+    const entries = Object.entries(this.fields).map(([name, width]) => {
+      if (width === undefined || width === null) {
+        return [name, this.maxColumnWidth(values, name) + 1];
+      } else {
+        return [name, width];
+      }
+    });
+    return Object.fromEntries(entries);
+  }
+
+  private maxColumnWidth(values: ColumnValues<T>[], name: string): number {
+    const col = values.map(v => v[name]);
+    const longest = col.reduce((a, b) => (a.length > b.length ? a : b));
+    return longest.length;
   }
 }
---
+++ src/shader-util/test/FormattedCsv.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "vitest";
+import { FormattedCsv } from "../FormattedCsv.js";
+
+test("static column widths", () => {
+  const fmt = new FormattedCsv({ name: 5, value: 6 });
+  const report = fmt.report([{ name: "foo", value: "bar" }]);
+  const expected = `
+ name, value
+  foo,   bar`.slice(1);
+  expect(report).toEqual(expected);
+});
+
+test("allow dynamic column widths", () => {
+  const fmt = new FormattedCsv({ name: undefined, value: 6 });
+  const report = fmt.report([{ name: "long_name", value: "bar" }]);
+  const expected = `
+      name, value
+ long_name,   bar`.slice(1);
+  expect(report).toEqual(expected);
+});
