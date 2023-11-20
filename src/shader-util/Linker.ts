a3543a844a7b0beb74dbbb8fe25f0343bfbee03f
lee mighdoll <lee@underneath.ca>
2023-11-19 21:49:12 -0800

    first test for #importReplace processing

--- src/shader-util/Linker.ts
+++ src/shader-util/Linker.ts
@@ -3,19 +3,35 @@ Parse a simple extension for wgsl that allows for linking shaders together.
 
 #export name
 #export name(param1, param2, ...)
-#end-export
+#endExport
 . export is the text of the rest of the file or until optional #end-export
 . params are optional, and are globally text replaced in the export text
 
 #import foo
 #importReplace foo
-#end-import
+#endImport
 . include the imported text
 
 */
 
-export function linkShaders(): string {
-  return "";
+/** register a module's exports so that imports can find it */
+export function registerModule(): void {
+  /* TODO */
+}
+
+/** parse shader text for imports, return wgsl with all imports injected */
+export function linkWgsl(src: string, registry: ModuleRegistry): string {
+  return processImportsRecursive(src, registry, [], []);
+}
+
+/** an imported module, for deduplicatio of imports */
+interface ImportModule {
+  name: string;
+  params: string[];
+}
+
+interface WgslModule {
+  exports: Export[];
 }
 
 interface Export {
@@ -26,16 +42,15 @@ interface Export {
 
 const optComment = /(\s*\/\/)?/;
 const exportDirective = /\s*#export\s+(?<export>[\w-]+)/;
-const importReplaceDirective = /\s*#importReplace\s+(?<export>[\w-]+)/;
+const importReplace = /\s*#importReplace\s+(?<import>[\w-]+)/;
+const endImport = /\s*#endImport/;
 const optParams = /\s*(\((?<params>[\w, ]*)\))?/;
 export const exportRegex = regexConcat(optComment, exportDirective, optParams);
-export const importReplaceRegex = regexConcat(
-  optComment,
-  importReplaceDirective,
-  optParams
-);
+export const importReplaceRegex = regexConcat(optComment, importReplace, optParams);
+export const endImportRegex = regexConcat(optComment, endImport);
 
-export function parseExports(src: string): Export[] {
+/** parse module text to find the exports */
+export function parseModule(src: string): WgslModule {
   let currentExport: Partial<Export> | undefined;
   let currentExportLines: string[] = [];
   const results: Export[] = [];
@@ -59,7 +74,61 @@ export function parseExports(src: string): Export[] {
     currentExport.src = currentExportLines.join("\n");
     results.push(currentExport as Export);
   }
-  return results;
+  return { exports: results };
+}
+
+function processImportsRecursive(
+  src: string,
+  registry: ModuleRegistry,
+  params: string[],
+  imported: ImportModule[]
+): string {
+  const out: string[] = [];
+  let importReplacing = false; // true while we're reading lines inside an importReplace
+  src.split("\n").forEach((line, lineNum) => {
+    const impReplace = line.match(importReplaceRegex); 
+    if (impReplace) {
+      console.assert(
+        importReplacing === false,
+        `found importReplace while parsing importReplace line: ${lineNum}`
+      );
+      const name = impReplace.groups!.import;
+      const params = impReplace.groups?.params?.split(",").map(p => p.trim()) ?? [];
+      const importModule = registry.getModule(name);
+      if (!importModule) {
+        console.error(
+          `importReplace module not found: ${name} at ${lineNum}\n>>\t${line}`
+        );
+      } else {
+        imported.push({ name, params });
+        const importSrc = importModule.src;
+        const importText = processImportsRecursive(importSrc, registry, params, imported);
+        out.push(importText);
+      }
+      importReplacing = true;
+    } else if (!importReplacing) {
+      out.push(line);
+    } else {
+      const endImport = line.match(endImportRegex);
+      if (endImport) {
+        importReplacing = false;
+      }
+    }
+  });
+  return out.join("\n");
+}
+
+export class ModuleRegistry {
+  private exports = new Map<string, Export>();
+
+  registerModule(src: string): void {
+    const module = parseModule(src);
+    module.exports.forEach(e => this.exports.set(e.name, e));
+  }
+
+  getModule(name: string): Export | undefined {
+    return this.exports.get(name);
+  }
 }
 
 function regexConcat(...exp: RegExp[]): RegExp {
--- src/shader-util/test/Linker.test.ts
+++ src/shader-util/test/Linker.test.ts
@@ -1,5 +1,11 @@
 import { expect, test } from "vitest";
-import { exportRegex, importReplaceRegex, parseExports } from "../Linker.js";
+import {
+  ModuleRegistry,
+  exportRegex,
+  importReplaceRegex,
+  linkWgsl,
+  parseModule,
+} from "../Linker.js";
 
 test("export regex w/o params", () => {
   const result = "// #export foo".match(exportRegex);
@@ -10,7 +16,6 @@ test("export regex w/o comment prefix", () => {
   const result = "#export foo".match(exportRegex);
   expect(result?.groups?.export).toBe("foo");
 });
-
 
 test("parse regex with params", () => {
   const result = "// #export foo(a, b, c)".match(exportRegex);
@@ -25,21 +30,50 @@ test("read simple export", () => {
         sum: f32,  
     }
   `;
-  const results = parseExports(exportPrefix + src);
-  expect(results.length).toBe(1);
-  const result = results[0];
+  const module = parseModule(exportPrefix + src);
+  expect(module.exports.length).toBe(1);
+  const result = module.exports[0];
   expect(result.name).toBe("binaryOp");
   expect(result.params).deep.equals(["Elem"]);
   expect(result.src).toBe(src);
 });
 
-test("parse simple importReplace", () => {
-  const src = "// #importReplace reduce-workgroup( param1, param2 )";
+test.only("parse importReplace w/params", () => {
+  const src = "// #importReplace reduceWorkgroup( param1, param2 )";
   const result = src.match(importReplaceRegex);
   expect(result?.groups?.params).toBe(" param1, param2 ");
-  expect(result?.groups?.export).toBe("reduce-workgroup");
+  expect(result?.groups?.import).toBe("reduceWorkgroup");
 });
 
+test("apply simple importReplace", () => {
+  const module = `
+  // #export reduceWorkgroup
+  fn reduceWorkgroup(localId: u32) {
+    // do reduce
+  }
+  `;
+
+  const src = `
+    // #importReplace reduceWorkgroup
+    fn reduceWorkgroup(localId: u32) {} 
+    // #endImport
+    reduceWorkgroup(localId); // call the imported function
+  `;
+  const registry = new ModuleRegistry();
+  registry.registerModule(module);
+
+  const linked = linkWgsl(src, registry);
+  expect(linked).includes("do reduce");
+  expect(linked).includes("call the imported function");
+});
+
+/*
+TODO
+ . 
+ . test importReplace with parameters
+ . test transitive imports
+ . test code gen import via template
+*/
 /*
 // #importReplace reduce-workgroup(work, Output, workgroupThreads)
 fn reduceWorkgroup(localId: u32) {} 
